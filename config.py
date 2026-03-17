9b04f8254e780da5bcf6151c3bfb47955a42ef3d
Erick_HP-Win <erick.baptista00@gmail.com>
2026-03-17 07:26:46 +0100

    No comments, i am getting crazy ahhhhhhhhhhhhhh

--- config.py
+++
@@ -1,9 +0,0 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///ponto24.db"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# Config email (para enviar código ao cliente)
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USER = "ponto24.ao@gmail.com"
-EMAIL_PASS = "ofbd akxi dabd idfc"
-
